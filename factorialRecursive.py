5e103d861635f1983b20f9cd5ec526cf2e5ee8de
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-04-26 16:56:51 +0300

    performed factorial using recursion & added comments

--- factorialRecursive.py
+++ factorialRecursive.py
@@ -1,7 +1,7 @@
 def factorialRecursive(n):
-    if n <= 1:
+    if n <= 1:  #functn can't call when number is 1 or below 1
         return n
     else:
-        return n * factorialRecursive(n - 1)
+        return n * factorialRecursive(n - 1)    #calls functn repeatedly minusing 1
 
-print(factorialRecursive(3))
+print(factorialRecursive(3))    #testing
